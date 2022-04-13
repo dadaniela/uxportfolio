fd3a79e2de409e3d8ddbf3664bcc04260fbb0ac4
Daniela <trindadedv@gmail.com>
2022-04-14 00:11:11 +0200

    background image and name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import background from './assets/under-construction.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+        <p
+          style={{
+            color: "white",
+            fontSize: 50,
+            fontFamily: "Barlow Condensed, sans-serif",
+          }}
+        >DANIELA TRINDADE</p>
     </div>
   );
 }
